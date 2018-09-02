63851eadf1d3bccaf6b583eba351f6788316bc9e
Josue Abraham <josuea@noukod.com>
2018-09-02 17:00:59 +0000

    automatically backed up by learn

--- lib/play.rb
+++ lib/play.rb
@@ -1,4 +1,4 @@
-## Helper Methods
+# Helper Methods
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -36,14 +36,10 @@ def turn(board)
 end
 
 # Define your play method below
-
 def play(board)
-
-  counter = 1
-
-  while counter <= 9
-    turn(board)
-    counter += 1
-  end
-
+i = 0
+while i < 9
+  i += 1
+  turn(board)
+end
 end
