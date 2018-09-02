152a410eb10a53bf94cf7da6b572beee42e9685f
Josue Abraham <josuea@noukod.com>
2018-09-02 16:32:06 +0000

    automatically backed up by learn

--- spec/01_cli_spec.rb
+++ spec/01_cli_spec.rb
@@ -2,7 +2,7 @@ describe './bin/play' do
   it 'evokes #play passing it `board` as an argument' do
     allow($stdout).to receive(:puts)
 
-    expect(self).to receive(:play).with(kind_of(board))
+    expect(self).to receive(:play).with(kind_of(play))
 
     run_file("./bin/play")
   end
